2dbff9175f998bb4d51abf08c27da3c6f5ca154c
Roland Rabien <figbug@gmail.com>
2022-03-07 19:05:08 -0800

    Revert "NamedPipe:   Fixed a deadlock on close"
    This reverts commit ea14d28bc9c6126da9fdc7663cebbc67a2dba739.

--- modules/juce_core/native/juce_posix_NamedPipe.cpp
+++ modules/juce_core/native/juce_posix_NamedPipe.cpp
@@ -246,17 +246,12 @@ private:
 
 void NamedPipe::close()
 {
-    {
-        ScopedReadLock sl (lock);
-
-        if (pimpl != nullptr)
-            pimpl->stopReadOperation = true;
-    }
-
     ScopedWriteLock sl (lock);
 
     if (pimpl != nullptr)
     {
+        pimpl->stopReadOperation = true;
+
         const char buffer[] { 0 };
         const auto done = ::write (pimpl->pipeIn.get(), buffer, numElementsInArray (buffer));
         ignoreUnused (done);
