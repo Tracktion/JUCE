a4e14054973827038e06c440e2d732957b636440
David Rowland <drowland96@gmail.com>
2026-07-28 13:31:19 +0100

    Tracktion: Fixed VST3 window positioning after constraining

--- modules/juce_audio_processors/format_types/juce_VST3PluginFormat.cpp
+++ modules/juce_audio_processors/format_types/juce_VST3PluginFormat.cpp
@@ -323,13 +323,15 @@ private:
                       bool,
                       bool) override
     {
+        auto pos = bounds.getPosition();
         auto rect = componentToVST3Rect (bounds);
+
         auto constrainedRect = rect;
         view->checkSizeConstraint (&constrainedRect);
 
         // Prevent inadvertent window growth while dragging; see componentMovedOrResized below
         if (constrainedRect.getWidth() != rect.getWidth() || constrainedRect.getHeight() != rect.getHeight())
-            bounds = vst3ToComponentRect (constrainedRect);
+            bounds = vst3ToComponentRect (constrainedRect).withPosition (pos);
     }
 
     //==============================================================================
